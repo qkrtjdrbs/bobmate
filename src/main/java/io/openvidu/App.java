fc84743262a93e53f2e44891a381fc7f4251447a
qkrtjdrbs <sldjaak0707@naver.com>
2022-03-28 19:45:27 +0900

    초기 세팅

--- src/main/java/io/openvidu/App.java
+++ src/main/java/io/openvidu/App.java
@@ -4,8 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-// 일단 db 설정 정보 안가져오기. 나중에 삭제 예정
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class App {
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
